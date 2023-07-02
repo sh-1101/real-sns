3f5162331e305dd886a8a79ccda5971183988b60
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-02 22:31:54 +0900

    feat: users.jsの記述をauth.jsに移動

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,4 @@
 const router = require("express").Router();
-const User = require("../models/User");
-
-//ユーザー登録
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = await new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    const user = await newUser.save();
-    return res.status(200).json(user);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
 
 // router.get("/", (req, res) => {
 //   res.send("user router");
