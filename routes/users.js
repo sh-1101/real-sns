c19bb4c72c40379ae629c78ffbb31b70e2759389
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-02 23:09:21 +0900

    feat: ユーザー情報を削除するAPIを作成

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,21 @@ router.put("/:id", async (req, res) => {
 });
 
 //ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザー情報が削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分のアカウントの時だけ情報を削除できます");
+  }
+});
+
 //ユーザー情報の取得
 
 // router.get("/", (req, res) => {
