2c05a599051668f41f9e9889c90713a726800604
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-04 22:05:01 +0900

    feat: 投稿にいいねを押すAPIを作成

--- routes/posts.js
+++ routes/posts.js
@@ -54,4 +54,31 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//特定の投稿にいいねを押す
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //まだ投稿にいいねが押されていなかったら
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json("投稿にいいねを押しました！");
+      //投稿にすでにいいねが押されていたら
+    } else {
+      //いいねしているユーザーIDを取り除く
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json("投稿のいいねを外しました");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
