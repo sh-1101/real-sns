945970313733b6b63849c9d3dafdc1ac74c464c3
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-10 22:26:48 +0900

    feat: Postコンポーネントの雛形を作成

---
+++ frontend/src/components/post/Post.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Post.css";
+import { MoreVert } from "@mui/icons-material";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
+          <span className="postUsername">Hiroki Sugiyama</span>
+          <span className="postDate">5分前</span>
+        </div>
+        <div className="postRight">
+          <MoreVert />
+        </div>
+        <div className="postCenter">
+          <span className="postText">SNSを自作中です。</span>
+          <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">5人がいいねを押しました</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postLikeCounter">4:コメント</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Timeline.css";
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 export default function Timeline() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {/* <Post /> */}
+        <Post />
       </div>
     </div>
   );
