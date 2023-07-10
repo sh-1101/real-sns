a618d78b1f1950efec2e7f2c78c0ecc1b731d1de
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-10 22:18:16 +0900

    style: Shareコンポーネントをスタイリング

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -15,25 +15,27 @@ export default function Share() {
           />
         </div>
         <hr className="shareHr" />
-        <div className="shareOptions">
-          <div className="shareOption">
-            <Image className="shareIcon" />
-            <span className="shareOptionText">写真</span>
+        <div className="shareButtons">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <Image className="shareIcon" htmlColor="blue" />
+              <span className="shareOptionText">写真</span>
+            </div>
+            <div className="shareOption">
+              <Gif className="shareIcon" htmlColor="hotpink" />
+              <span className="shareOptionText">GIF</span>
+            </div>
+            <div className="shareOption">
+              <Face className="shareIcon" htmlColor="green" />
+              <span className="shareOptionText">気持ち</span>
+            </div>
+            <div className="shareOption">
+              <Analytics className="shareIcon" htmlColor="red" />
+              <span className="shareOptionText">投票</span>
+            </div>
           </div>
-          <div className="shareOption">
-            <Gif className="shareIcon" />
-            <span className="shareOptionText">GIF</span>
-          </div>
-          <div className="shareOption">
-            <Face className="shareIcon" />
-            <span className="shareOptionText">気持ち</span>
-          </div>
-          <div className="shareOption">
-            <Analytics className="shareIcon" />
-            <span className="shareOptionText">投票</span>
-          </div>
+          <button className="shareButton">投稿</button>
         </div>
-        <button className="shareButton">投稿</button>
       </div>
     </div>
   );
