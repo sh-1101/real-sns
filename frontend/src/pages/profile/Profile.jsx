6b060062dc76a503ecc2bd93fbb3eecef26c5b19
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-15 22:54:18 +0900

    feat: プロフィールページの雛形作成

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -11,8 +11,30 @@ export default function Profile() {
       <Topbar />
       <div className="profile">
         <Sidebar />
-        <Timeline />
-        <Rightbar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                src="/assets/post/3.jpeg"
+                alt=""
+                className="profileCoverImg"
+              />
+              <img
+                src="/assets/person/7.jpeg"
+                alt=""
+                className="profileUserImg"
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Hiroki Sugiyama</h4>
+              <span className="profileInfoDesc">へっぽこプログラマー</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Timeline />
+            <Rightbar />
+          </div>
+        </div>
       </div>
     </>
   );
