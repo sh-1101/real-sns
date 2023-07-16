3e6e481b4090560e11aaa3fe07743d5c285a1448
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-16 10:21:11 +0900

    feat: プロフィールのRightbarの雛形を作成

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -44,7 +44,60 @@ export default function Rightbar({ profile }) {
   };
 
   const ProfileRightbar = () => {
-    return <>profileのRightbarです</>;
+    return (
+      <>
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+          <h4 className="rightbarTitle">あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img
+                src="/assets/person/1.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Hiroki Sugiyama</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="/assets/person/2.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Yamaki</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="/assets/person/3.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Koga</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="/assets/person/4.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Matukubo</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="/assets/person/5.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Kikukawa</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   };
   return (
     <div className="rightbar">
