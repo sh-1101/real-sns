56bd2658c4d5f5779d6121f4a95d3d9fa395cbc6
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-17 11:51:19 +0900

    feat: ログインコンポーネント右の雛形作成

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -9,7 +9,20 @@ export default function Login() {
           <h3 className="loginLogo">Real SNS</h3>
           <span className="loginDesc">本格的なSNSを自分の手で</span>
         </div>
-        <div className="loginRight">ああああ</div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <p className="loginMessage">ログインはこちら</p>
+            <input type="text" className="loginInput" placeholder="Eメール" />
+            <input
+              type="text"
+              className="loginInputPassword"
+              placeholder="パスワード"
+            />
+            <button className="loginButton">ログイン</button>
+            <span className="loginForgot">パスワードをお忘れの方</span>
+            <button className="loginRegisterButton">アカウント作成</button>
+          </div>
+        </div>
       </div>
     </div>
   );
