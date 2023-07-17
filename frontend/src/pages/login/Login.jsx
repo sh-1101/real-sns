7026f951613ba1f42eed47fe2efeb0e06a6e2be5
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-17 11:46:46 +0900

    feat: ログインコンポーネント左を作成

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Real SNS</h3>
+          <span className="loginDesc">本格的なSNSを自分の手で</span>
+        </div>
+        <div className="loginRight">ああああ</div>
+      </div>
+    </div>
+  );
+}
