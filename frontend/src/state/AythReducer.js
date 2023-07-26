5be331493e76e3427ce77bfa4bedf6cd4a403803
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-07-26 23:16:14 +0900

    feat: ユーザーの初期状態の定義とuseReducerを使う

--- frontend/src/state/AythReducer.js
+++ frontend/src/state/AythReducer.js
@@ -18,5 +18,9 @@ const AuthReducer = (state, action) => {
         isFetching: false,
         error: action.payload,
       };
+    default:
+      return state;
   }
 };
+
+export default AuthReducer;
