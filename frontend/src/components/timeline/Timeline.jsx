8304b381f0573d78b1c150171a45fc9abc7dee11
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-09 22:57:17 +0900

    feat: ログイン中のユーザーのタイムラインを表示

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Timeline.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
 // import { Posts } from "../../dummyData";
 import axios from "axios";
+import { AuthContext } from "../../state/AuthContext";
 
 export default function Timeline({ username }) {
   const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = username
         ? await axios.get(`/posts/profile/${username}`)
-        : await axios.get("/posts/timeline/64a178f177cad556c5c5a06f");
+        : await axios.get(`/posts/timeline/${user._id}`);
       setPosts(response.data);
     };
     fetchPosts();
-  }, [username]);
+  }, [username, user._id]);
   return (
     <div className="timeline">
       <div className="timelineWrapper">
