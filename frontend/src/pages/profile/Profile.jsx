44a38be07bf69a183e191d03a6b5b4494dbd609b
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-15 23:08:22 +0900

    fix: Profileコンポーネントの画像パス修正

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -35,7 +35,8 @@ export default function Profile() {
               />
               <img
                 src={
-                  user.profilePicture || PUBLIC_FOLDER + "/person/noAvatar.png"
+                  PUBLIC_FOLDER + user.profilePicture ||
+                  PUBLIC_FOLDER + "/person/noAvatar.png"
                 }
                 alt=""
                 className="profileUserImg"
