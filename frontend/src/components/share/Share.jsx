f9216f1fac9002f59cf9a4b09add1ec3c841f75e
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-16 19:33:41 +0900

    feat: 投稿用APIを叩いてタイムラインに投稿

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,11 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import "./Share.css";
 import { Analytics, Face, Gif, Image } from "@mui/icons-material";
 import { AuthContext } from "../../state/AuthContext";
+import axios from "axios";
 
 export default function Share() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useContext(AuthContext);
+  const desc = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    try {
+      await axios.post("/posts", newPost);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="share">
       <div className="shareWrapper">
@@ -23,10 +42,11 @@ export default function Share() {
             type="text"
             className="shareInput"
             placeholder="今何してるの？"
+            ref={desc}
           />
         </div>
         <hr className="shareHr" />
-        <div className="shareButtons">
+        <form className="shareButtons" onSubmit={(e) => handleSubmit(e)}>
           <div className="shareOptions">
             <div className="shareOption">
               <Image className="shareIcon" htmlColor="blue" />
@@ -45,8 +65,10 @@ export default function Share() {
               <span className="shareOptionText">投票</span>
             </div>
           </div>
-          <button className="shareButton">投稿</button>
-        </div>
+          <button className="shareButton" type="submit">
+            投稿
+          </button>
+        </form>
       </div>
     </div>
   );
