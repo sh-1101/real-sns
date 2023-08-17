8233e63fe4260d993b98f42aa883c5ff734d6201
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-17 21:47:06 +0900

    不要な記述を削除

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Timeline.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
-// import { Posts } from "../../dummyData";
 import axios from "axios";
 import { AuthContext } from "../../state/AuthContext";
 
