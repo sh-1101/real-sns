bb6fa7b73e3bd7a1c4d52ed9705588f6966ca7a9
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-20 16:39:10 +0900

    feat: multerをインストール

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const uploadRoute = require("./routes/upload");
 const PORT = 5000;
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/upload", uploadRoute);
 
 app.get("/", (req, res) => {
   res.send("hello express");
