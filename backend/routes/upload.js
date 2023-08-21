03e53b2fb60cee564231aa93d727bd0059bfcdd0
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-08-21 23:49:57 +0900

    feat: 画像アップロード用APIを作成

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const multer = require("multer");
 
+const upload = multer();
+//画像アップロード用API
+router.post("/", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("画像アップロードに成功しました！");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
